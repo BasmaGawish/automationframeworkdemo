3780544af0afb7e1666901900e79858a9f42635d
Basma Gawish <Bosygawish85@gmail.com>
2016-07-01 22:31:07 +1000

    Automation Framework Demo , Project Skeleton

---
+++ AutomationFrameworkDemo.Tests/VerifySeleniumTests.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace AutomationFrameworkDemo.Tests
+{
+    [TestFixture]
+    public class VerifySeleniumTests
+    {
+        [Test]
+        public void VerifySelenium()
+        {
+            IWebDriver driver=new ChromeDriver();
+            driver.Navigate().GoToUrl("http://localhost:52046/");
+            driver.Dispose(); //required to close the web driver
+        }
+    }
+}
---
+++ AutomationFrameworkDemo/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("AutomationFrameworkDemo")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AutomationFrameworkDemo")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("74216e15-404c-425a-9f83-fadace61ed4b")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
